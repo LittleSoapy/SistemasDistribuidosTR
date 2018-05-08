5de8dd12eaef40cf67e6a90716ae740a4e001ff6
Zengor <zengor@hotmail.com>
2018-05-08 03:46:48 -0300

    Fazer `/conectar` aceitar argumentos opcionais

--- src/cliente/Menu.java
+++ src/cliente/Menu.java
@@ -17,13 +17,11 @@ public class Menu {
 		System.out.println("escreva algo ou use o comando '/help para mais informacoes'");
 		while (!sair) {
 			String msg = teclado.nextLine();
+			String[] msg_parts = msg.split(" ");
 			if (msg.charAt(0) == '/') {
-				switch (msg) {
+				switch (msg_parts[0]) {
 				case "/conectar":
-					System.out.println("Escrever IP de outro Servidor:");
-					String ip = teclado.nextLine();
-					conectar(ip, 7077);
-					teclado.nextLine();
+					handle_connect(msg_parts, teclado);
 					break;
 				case "/sair":
 					sair = true;
@@ -45,6 +43,25 @@ public class Menu {
 
 		}
 		teclado.close();
+	}
+
+	private void handle_connect(String[] args, Scanner teclado) {
+		String ip = null;
+		int port = 7077;
+		if (args.length == 0) {
+			System.out.println("Escrever IP de outro Servidor:");
+			ip = teclado.nextLine();
+		} else if (args.length == 1) {
+			ip = args[1];
+		} else if (args.length == 2) {
+			ip = args[1];
+			port = Integer.parseInt(args[2]);
+		} else {
+			System.out.println("--Número incompatível de argumentos");
+			return;
+		}
+		conectar(ip, port);
+		teclado.nextLine();
 	}
 
 	public void conectar(String ip, int porta) {
